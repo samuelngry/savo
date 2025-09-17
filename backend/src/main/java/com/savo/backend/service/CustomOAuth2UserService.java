a2d455f7032a0b563bdb07aa98f00fc4a0849848
samuelngry <samuelngry00@gmail.com>
2025-09-17 13:17:01 +0800

    feat(o-auth): add load user for users who log in with Google

---
+++ backend/src/main/java/com/savo/backend/service/CustomOAuth2UserService.java
@@ -0,0 +1,25 @@
+package com.savo.backend.service;
+
+import com.savo.backend.repository.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
+
+        return processOAuth2User(oAuth2UserRequest, oAuth2User);
+    }
+}
