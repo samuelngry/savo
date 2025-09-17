d50803819979d9aa08fe2d0f559e94ed391218c9
samuelngry <samuelngry00@gmail.com>
2025-09-17 14:05:31 +0800

    feat(o-auth): add OAuth2UserInfoFactory to support Google login only

--- backend/src/main/java/com/savo/backend/config/OAuth2SecurityConfig.java
+++ backend/src/main/java/com/savo/backend/config/OAuth2SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.savo.backend.config;
 
+import com.savo.backend.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
