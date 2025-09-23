e4d939af2b3def1493f418fca4f88b8d88dd252e
samuelngry <samuelngry00@gmail.com>
2025-09-23 16:21:54 +0800

    feat(landing): add hero section with image, header and text

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,10 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
-import Features from "@/components/Features";
-import HowItWorks from "@/components/HowItWorks";
-import CTA from "@/components/CTA";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main>
       <Hero />
-      <Features />
-      <HowItWorks />
-      <CTA />
     </main>
   );
 }
--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,3 +1,33 @@
+import Image from "next/image";
+
 export default function Hero() {
-    
+    return (
+        <section className="w-full min-h-screen flex items-center justify-center bg-white px-6 md:px-16">
+            <div className="max-w-7xl w-full flex flex-col md:flex-row items-center gap-10">
+
+                {/* Text */}
+                <div className="flex-1">
+                    <h1 className="text-7xl md:text-6xl font-bold mb-6 mt-2">
+                        Upload Statements, Instant Insights
+                    </h1>
+                    <p className="text-lg mb-6">
+                        Upload your bank statements in seconds. Get instant insights into your spending, and no account linking needed.
+                    </p>
+                    <button className="bg-slate-950 text-white font-bold px-6 py-3 rounded-lg cursor-pointer">Get Started</button>
+                </div>
+
+                {/* Image */}
+                <div className="flex-1">
+                    <Image 
+                        src="/landing.png"
+                        alt="Savo App"
+                        width={500}
+                        height={400}
+                        priority
+                    />
+                </div>
+            </div>
+
+        </section>
+    )
 }
