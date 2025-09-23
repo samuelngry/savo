1aaeec263a072f125be09d4fe3359c0c4a7d7836
samuelngry <samuelngry00@gmail.com>
2025-09-23 23:47:46 +0800

    feat(landing): add navbar for login/signup and more about savo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,10 +3,12 @@ import FAQ from "@/components/FAQ";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
       <Hero />
       <HowItWorks />
       <Features />
---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavBar() {
+    return (
+        <nav className="w-full shadow-md">
+            <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+                {/* Logo */}
+                <div className="flex items-center">
+                    <Link href="/">
+                        <Image
+                        src="/logo.png"
+                        alt="Savo Logo"
+                        width={60}
+                        height={60}
+                        className="cursor-pointer"
+                        />
+                    </Link>
+                </div>
+
+                <div className="hidden md:flex gap-8 text-sm font-medium text-slate-950">
+                    <Link href="/why-savo">Why Savo?</Link>
+                    <Link href="/contact">Contact</Link>
+                    <Link href="/pricing">Pricing</Link>
+                </div>
+
+                <div className="flex gap-8 text-sm items-center">
+                    <Link href="/login">
+                        <button className="text-slate-950 font-medium hover:underline cursor-pointer">Login</button>
+                    </Link>
+                    <Link href="/signup">
+                        <button className="bg-slate-950 text-white px-4 py-2 rounded-lg font-semibold cursor-pointer">
+                            Sign Up
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+  );
+}
