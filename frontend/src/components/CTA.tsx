b5cca1c669b1be5fe3c7aa353a0f5edf1012bde3
samuelngry <samuelngry00@gmail.com>
2025-09-24 00:01:40 +0800

    chore: update favicon to savo logo

--- frontend/src/components/CTA.tsx
+++ frontend/src/components/CTA.tsx
@@ -6,14 +6,14 @@ export default function CTA() {
             className="relative w-full h-[400px] bg-center bg-cover bg-no-repeat"
             style={{ backgroundImage: "url('/cta.jpg')" }} 
             >
-            <div className="absolute inset-0 bg-black/70 z-0" />
+            <div className="absolute inset-0 bg-black/100 z-0" />
 
             <div className="relative z-10 flex flex-col items-center justify-center text-center h-full px-6">
                 <h2 className="text-5xl md:text-5xl font-bold text-white mb-4">
                     Ready to Take Control of Your Finances?
                 </h2>
                 <p className="text-lg md:text-xl text-white mb-8 max-w-2xl">
-                    Upload your bank statements and get instant insights without linking any accounts
+                    Upload your bank statements and get instant insights without linking any accounts.
                 </p>
                 <Link href="/get-started">
                 <button className="bg-white text-slate-950 font-bold px-6 py-3 rounded-lg hover:bg-gray-100 transition">
